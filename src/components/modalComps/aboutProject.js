a9fe5f6b8570f505e78bbdfccd559e450f539d28
Antonio832 <antoniomedina832@outlook.com>
2023-04-13 12:12:18 -0700

    aboutProject updated

--- src/components/modalComps/aboutProject.js
+++ src/components/modalComps/aboutProject.js
@@ -8,6 +8,12 @@ function AboutProject() {
     return (
         <div className='modalAbtPro'>
             <h2>About this project</h2>
+            <div>
+                I began this project as a way to challenge my creativity and my skills. This project is 
+                writen in React to manage the UI and API calls as well as some different libraries, before I started this 
+                project, I had very little experience with React so this was a great way to 
+                
+            </div>
         </div>
     )
 }
