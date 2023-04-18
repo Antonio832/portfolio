1452dcd1f764b6154a8e958228585f6beb99797a
Antonio832 <antoniomedina832@outlook.com>
2023-04-18 15:46:52 -0700

    projects modal updated

--- src/components/modalComps/aboutProject.js
+++ src/components/modalComps/aboutProject.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../styles/modalStyles/modal.css'
 import '../../styles/modalStyles/aboutP.css'
 
@@ -15,25 +15,41 @@ function AboutProject() {
                 skills and learn new ways to implement what I already knew.
             </div>
 
-            <div style={{marginTop: '1rem'}}>
+            <div style={{marginTop: '1rem', paddingBottom: '5rem'}}>
                 This project was written in React for managing the UI. I divided the page into 4 panes.
                 <br/>
-                The first pane is uses a quote api, to display famous quotes that can motivate whoever decides
+                The top left pane is uses a quote api, to display famous quotes that can motivate whoever decides
                  to visit this page, there's also a copy button, just in case anyone would like to copy the quote. 
                 <br/>
                 <br/>
-                    <a href='https://type.fit/api/quotes/' target='blank'>
-                        🔗 Here is the Api endpoint
-                    </a>
+                <a href='https://type.fit/api/quotes/' target='blank'>
+                    🔗 Here is the Api endpoint
+                </a>
                 <br/>
                 <br/>
-                The second pane has a showcase of the various subject, some of them are programming related and others 
+                The top right pane has a showcase of the various subject, some of them are programming related and others 
                 art projects, each one has a modal associated to it, all of the styling is documented in the github repo for this
                 site.
                 <br/>
                 <br/>
                 <a href='https://github.com/Antonio832/portfolio' target='blank'>
                     🔗 Check the repo here
+                </a>
+                <br/>
+                <br/>
+                The bottom left one has my full name with some animations to reveal some buttons to display the "About me" section
+                as well as the button for these same modal.
+                <br/>
+                <br/>
+                The bottom right pane has all my contact info and all the places where I am active. Feel free to contact me however you'd like.
+                <br/>
+                <br/>
+                And last but not least, the background was made with the p5.js library, its drawing a flocking simulation, also known as Boids which simulates the way birds fly in groups withouth hitting eachother.
+                 It also draws where the cursor is and it replaces it with a circle with a tail dragging behind it. The full code for the sketch is in the repo for this project.
+                <br/>
+                <br/>
+                <a href='https://p5js.org' target='blank'>
+                    🔗 Check out p5.js
                 </a>
             </div>
         </div>
--- src/components/modalComps/projects.js
+++ src/components/modalComps/projects.js
@@ -20,27 +20,23 @@ function ProjectsModal() {
                     The sessions are recorded and uploaded to a private folder on Google drive for the students to review each class. The topics of the course include: Angular
                     Angular Material, Firebase (Firestore, Authentication and Hosting).\n All sessions are private and completly on the hands of the student, so it is very common to also 
                     touch subjects like Git, general Javascript and programming practices, Markup language, and even other frameworks. Since the student has full control over the course, the duration and the topics boarded on that session 
-                    are set by the student.`,
+                    are set by the student. The showcase can be the demo, but also we can see other students projects in action.`,
             canShow: true,
             link: '//dmo-listas.web.app'
         },
         {
             title: 'Pokemap*',
-            desc: `I developed a Trello style application as a demo for an online class where the students recreate this app from start to finish.
-                    The sessions are recorded and uploaded to a private folder on Google drive for the students to review each class. The topics of the course include: Angular
-                    Angular Material, Firebase (Firestore, Authentication and Hosting).\n All sessions are private and completly on the hands of the student, so it is very common to also 
-                    touch subjects like Git, general Javascript and programming practices, Markup language, and even other frameworks. Since the student has full control over the course, the duration and the topics boarded on that session 
-                    are set by the student.`,
+            desc: `This app is designed to display events from the famous game Pokemon Go, 
+                    it shows a dynamic timezone map where each timezone can have can have a number of events happening, 
+                    it also shows the type, name of the event, the real hour of the timezone and the best places to play the game. 
+                    Currently this app no longer works due to timezones changing, since I design the maps from scratch it is very difficult to keep up with the new timezones, and decided to leave it as a showcase due to the amount of logic implemented in this project.`,
             canShow: true,
             link: '//www.pokemap.live'
         },
         {
             title: 'LCC-HUB Admin Dashboard',
-            desc: `I developed a Trello style application as a demo for an online class where the students recreate this app from start to finish.
-                    The sessions are recorded and uploaded to a private folder on Google drive for the students to review each class. The topics of the course include: Angular
-                    Angular Material, Firebase (Firestore, Authentication and Hosting).\n All sessions are private and completly on the hands of the student, so it is very common to also 
-                    touch subjects like Git, general Javascript and programming practices, Markup language, and even other frameworks. Since the student has full control over the course, the duration and the topics boarded on that session 
-                    are set by the student.`,
+            desc: `A project created for managing data and posts displayed on a "hub" from my University, this project is an admin dashboard with hierarchical authentication where the users
+                    manage data from students to be displayed at the hub, the data can be imported from an Excel template and will automaticaly add the data to a NoSQL database in Firebase.`,
             canShow: false,
         },
     ]
